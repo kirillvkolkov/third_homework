0c26b973ddc70da2a041dc6466cfb2b865ce4b04
kirillvkolkov <86800844+kirillvkolkov@users.noreply.github.com>
2021-07-30 16:30:18 +0300

    Add files via upload

--- task_8.py
+++ task_8.py
@@ -3,12 +3,13 @@ inp_height = int(input())
 
 inp_splitted = inp_str.split(' ')
 
-list = sorted([int(x) for x in inp_splitted])
+list = sorted([int(x) for x in inp_splitted], reverse=True)
 
 
 for height in range(0, len(list) - 1):
-    if list[height] > inp_height:
+    if list[height] < inp_height:
         list.insert(height, inp_height)
+        break
 
 
 print(list)
